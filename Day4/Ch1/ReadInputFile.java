e5ac8b34d10cfb71b3ee31d1da858ff143d086be
Riccardo Ragazzini <riccardo.ragazzini4@studio.unibo.it>
2025-12-08 17:02:36 +0100

    Solved day 4

---
+++ Day4/Ch1/ReadInputFile.java
@@ -0,0 +1,38 @@
+package Day4.Ch1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadInputFile{
+    private File inputFile;
+    private int lineLength;
+
+    public ReadInputFile(String fileName){
+        inputFile = new File(fileName);    
+    }
+
+    public ArrayList<String> scanFile(){
+        try (Scanner reader=new Scanner(this.inputFile)){
+            ArrayList<String>  values =new ArrayList<String>(mapFileToListOfjoltage(reader));
+            return values;
+        }catch (FileNotFoundException e ){
+            System.out.print(e);
+            return null;
+        }
+    }
+
+    private ArrayList<String> mapFileToListOfjoltage(Scanner file){
+        ArrayList<String> array = new ArrayList<String>();
+        while (file.hasNextLine()){
+
+            array.addAll(Arrays.asList(file.nextLine().split("")));
+        }
+        return array;
+    }
+    public int getLineLength(){
+        return lineLength;
+    }
+}
